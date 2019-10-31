0a53518c30306b5f53f98f79e07d194a8c5f0a49
Aed3r <xxgoffi22xx@gmail.com>
2019-10-31 15:48:31 +0100

    Added screen.h

---
+++ Modules/Screen/J_screen.h
@@ -0,0 +1,44 @@
+#ifndef SCREEN
+#define SCREEN
+
+#include <stdlib.h>
+#include <MLV/MLV_all.h>
+
+#include "J_objects.h"
+
+#define MAXMSGCHARS 200
+#define MAXMENUCHARS 50
+
+/*
+ * Displays all platforms
+ */
+void dispPlats(platform plt[PLATFORMS]);
+
+/*
+ * Displays all birds
+ */
+void dispBirds(bird brd[MAXINSTANCES]);
+
+/*
+ * Displays both players lives and score
+ */
+void dispStatus(bird brd[MAXINSTANCES]);
+
+/*
+ * Displays the message passed as parameter
+ */
+void dispText(char msg[MAXMSGCHARS], int length);
+
+/*
+ * Displays the current frame according to the games state
+ */
+void dispFrame(platform plt[PLATFORMS], bird brd[MAXINSTANCES]);
+
+/*
+ * Displays a menu with buttons passed as parameters
+ * Returns the index of the clicked button
+ * Can display an image if given a valid path
+ */
+int dispMenu(char *button[MAXMENUCHARS], char *imgPath);
+
+#endif
