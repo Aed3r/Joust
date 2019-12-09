06ca02c31b0386753092b917a20140305d0649f4
Marwan <Darkmar74@outlook.com>
2019-12-09 02:16:11 -0800

    Ia.h defined and starting function writing

--- Modules/IA/J_ia.c
+++ Modules/IA/J_ia.c
@@ -1,5 +1,13 @@
 #include "J_ia.h"
 
-void moveAggIa(birds b){
-	
+int isBelow(birds *b, int n){
+	if(b->brd[0].point.y > b->brd[n].point.y) return 1;
+	return 0;
+}
+
+void movePassiveIa(birds *b){
+	int i;
+	for(i=2;i<b.l;i++){
+		if(isBelow(b, i)) b->brd[i].flapped = 1;
+	}
 }
--- Modules/IA/J_ia.h
+++ Modules/IA/J_ia.h
@@ -1,30 +1,13 @@
 #ifndef IA
 #define IA
 
+/*Renvoie 1 si l'oiseau est en dessou du joueur1 , 0 sinon */
+int isBelow(birds b, int n);
+
 /*Gère l'ia des enemis peu agressifs*/
 void movePassievIa(birds b);
 
 /*Idem mais pour des enemis agressifs*/
 void moveAggIa(birds b);
 
-/*Gère la joute entre deux IA, si deux IA sont sur le point
- *de jouter elle essaie de s'esquiver, celle de droite (choix arbitraire)
- *devras essayer de passer en dessous de l'autre
- */
-void IAJoust(bird a, bird b);
-
-/*Renvoie l'altitude d'un oiseau b (parmis les trois prédefinies) 
- *Ou une valeur intermédiare si l'oiseau est entre deux altitudes
- * Air : 4, mid-air : 2, ground : 0
- */
-int Altitude(bird b);
-
-/*Renvoie 1 si l'oiseau est en dessou du joueur, 0 sinon*/
-int isBelow(bird b);
-
-/*Gère le comportement d'un oiseau qui augmente d'une altitude*/
-void goUp(bird b);
-
-/*Gère le comportement d'un oiseau qui descend d'une altitude*/
-void goDown(bird b);
 #endif
